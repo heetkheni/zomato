291ecc1c7aa33a7a90472358aabe7d7491950fbd
Heet Kheni <heetkheni47@gmail.com>
2024-09-24 18:41:51 +0530

    init

---
+++ src/main/java/com/ZomatoProjectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoProjectApplication.class, args);
+	}
+
+}
